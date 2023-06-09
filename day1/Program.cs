f47948eab674851399520619f8a5d135a1f1027e
Josh Tan <38924154+joshtn@users.noreply.github.com>
2023-06-09 13:29:18 +0200

    naive sol day1 part2

--- day1/Program.cs
+++ day1/Program.cs
@@ -9,21 +9,44 @@ namespace day1
         static void Main(String[] args)
         {
             var ints = File.ReadAllLines("input.txt")
-            .Select(s => Int32.Parse(s)).ToList();
+                .Select(s => Int32.Parse(s)).ToList();
+            Console.WriteLine(SolvePart2(ints));
+        }
+        
+        static int SolvePart2(List<int> ints)
+        {
             for (int i = 0; i < ints.Count; i++)
             {
-               var first = ints[i];
                for (int j = i+1; j < ints.Count; j++)
                {
-                var second = ints[j];
+                for (int k = 0; k < ints.Count; k++)
+                {
+                    if (ints[i] + ints[j] + ints[k] == 2020)
+                    {
+                        Console.WriteLine("Found it!");
+                        return (ints[i] * ints[j] * ints[k]);
+                    }
+                }
+              } 
+            }
+            return 0;
+        }
+        
+        static int SolvePart1(List<int> ints)
+        {
+            for (int i = 0; i < ints.Count; i++)
+            {
+               for (int j = i+1; j < ints.Count; j++)
+               {
                 if (ints[i] + ints[j] == 2020)
                 {
                     Console.WriteLine("Found it!");
-                    Console.WriteLine(ints[i] * ints[j]);
-                    return;
+                    return (ints[i] * ints[j]);
                 }
                } 
             }
+            return 0;
         }
+
     }
 }
