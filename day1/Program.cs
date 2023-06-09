67fc491cb9b2f97105235ff2ddffbe6310b50d40
Josh Tan <38924154+joshtn@users.noreply.github.com>
2023-06-09 12:25:43 +0200

    naive sol day1 part1

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            var ints = File.ReadAllLines("input.txt")
+            .Select(s => Int32.Parse(s)).ToList();
+            for (int i = 0; i < ints.Count; i++)
+            {
+               var first = ints[i];
+               for (int j = i+1; j < ints.Count; j++)
+               {
+                var second = ints[j];
+                if (ints[i] + ints[j] == 2020)
+                {
+                    Console.WriteLine("Found it!");
+                    Console.WriteLine(ints[i] * ints[j]);
+                    return;
+                }
+               } 
+            }
+        }
+    }
+}
