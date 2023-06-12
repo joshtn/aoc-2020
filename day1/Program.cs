315ae7eec876c5c746e71938a6eedd3f9412b2eb
Josh Tan <38924154+joshtn@users.noreply.github.com>
2023-06-13 00:19:02 +0200

    recursive solution works for part1 and part2

--- day1/Program.cs
+++ day1/Program.cs
@@ -8,9 +8,33 @@ namespace day1
     {
         static void Main(String[] args)
         {
+            try
+            {
+
             var ints = File.ReadAllLines("input.txt")
                 .Select(s => Int32.Parse(s)).ToList();
-            Console.WriteLine(SolvePart2(ints));
+
+                var howManyIntsToSum = 3;
+                var requiredSum = 2020;
+
+            var solution = Solve(ints, howManyIntsToSum, requiredSum);
+            Console.WriteLine(solution);
+            } catch(Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+        
+        // recursive solution for fun
+        static int Solve(IEnumerable<int> ints, int howManyIntsToSum, int requiredSum)
+        {
+            if (howManyIntsToSum == 0 || ! ints.Any()) return 0;
+            var head = ints.First();
+            var tail = ints.Skip(1);
+            if (howManyIntsToSum == 1 && head == requiredSum) return(head);
+            var result = head * Solve(tail, howManyIntsToSum - 1, requiredSum - head);
+            if (result != 0) return result;
+            return Solve(tail, howManyIntsToSum, requiredSum);
         }
         
         static int SolvePart2(List<int> ints)
