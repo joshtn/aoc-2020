94354f1e6073ec25ea8f85e1f1e0ef005bef847b
Josh Tan <38924154+joshtn@users.noreply.github.com>
2023-06-14 23:04:30 +0200

    cleanup sol part1:

--- day2/Program.cs
+++ day2/Program.cs
@@ -1,17 +1,6 @@
 ﻿string[] input = System.IO.File.ReadAllLines("input.txt");
-string dummytxt = @"
-1-3 a: abcde
-1-3 b: cdefg
-2-9 c: ccccccccc
-";
 int output = 0;
 
-//string[] dummysplit = dummytxt.Split("/n");
-
-string[] dummysplit = dummytxt.Split(
-    new string[] { Environment.NewLine },
-    StringSplitOptions.None
-);
 
 
 foreach (string line in input)
@@ -25,8 +14,6 @@ foreach (string line in input)
     var letter = char.Parse(substring[1].Substring(0, 1));
 
     var password = substring[2];
-    
-    Console.WriteLine($"min {min} max {max} letter {letter} password {password}"); // printing first line correct then stopping for some reason whyyyy
     
     var count = password.Count(c => c == letter);
     if (count >= min && count <= max)
